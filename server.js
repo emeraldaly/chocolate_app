3a609fc7fb58569c385e83f8e283d9541ca4c4ed
Alyson <emerald.aly@gmail.com>
2016-02-20 10:34:26 -0500

    built out server.js further

--- server.js
+++ server.js
@@ -1,26 +1,25 @@
 var express = require('express');
-var exphbs = require('express-handlebars');
-var Sequelize = require('sequelize');
-var session = require("express-session");
-
-var connection = new Sequelize('chocolate_db', 'root', {
-
-});
 var app = express();
-
 var PORT = process.env.PORT || 8080;
 
-app.set('view engine', 'handlebars');
 
-app.engine('handlebars', expressHandlebars({
+var Sequelize = require('sequelize');
+var connection = new Sequelize('chocolate_db', 'root');
+
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
   defaultLayout: 'main'
 }));
 
+app.set('view engine', 'handlebars');
+
 //routes
 app.get('/', function(req, res) {
-  res.render('chocolate_db');
+  res.render('home');
 });
 
+connection.sync().then(function() {
 app.listen(PORT, function() {
   console.log("Listening on port %s", PORT); 
+  });
 });
